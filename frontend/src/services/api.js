e46f9a8d677d9ddff589887c2e35530c966cdd19
Harshith2243 <aripelliharshith123@gmail.com>
2026-06-23 12:24:51 +0530

    Updated backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://e-commerce-backend-500r.onrender.com/api",
 });
 
 export default API;
